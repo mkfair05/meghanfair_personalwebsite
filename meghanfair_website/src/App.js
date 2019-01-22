4c8faf4b93fcd5ac45b6bf47efb7d2ae6fcec038
MeghanFair <mkfair05@gmail.com>
2019-01-22 14:12:14 -0800

    Set up, and start of styling of new website.

--- meghanfair_website/src/App.js
+++ meghanfair_website/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import MainPage from './MainPage';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
       
         <head className="App-header">
+          <title>Meghan Fair React Website</title>
+          
           {/* <!-- Import Google Icon Font --> */}
           <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
           {/* <!-- Import materialize.css --> */}
           <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css" rel="stylesheet"></link>
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"></link>
+
         </head>
 
         <body>
@@ -19,8 +25,9 @@ class App extends Component {
           <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
           {/* <!-- And then your bundled js --> */}
           <script src="path/to/your/bundle.js"></script>
+          <MainPage/>
         </body>
-        
+
       </div>
     );
   }
---
+++ meghanfair_website/src/MainPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class MainPage extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Meghan Fair</h1>
+                    <i class="fas fa-home"></i>
+                    <i class="fab fa-github"></i>
+                    <i class="fas fa-file-pdf"></i>
+                    <i class="fab fa-linkedin-in"></i>
+                </header>
+
+                <h1>Hello World</h1>
+
+            </div>
+        );
+    }
+
+}
+export default MainPage;
