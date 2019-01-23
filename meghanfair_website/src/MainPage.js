9838332a352b392a9048260340aa41cb64131ccd
MeghanFair <mkfair05@gmail.com>
2019-01-23 14:25:21 -0800

    CSS changes, set icons as buttons, spacing changes.

--- meghanfair_website/src/MainPage.js
+++ meghanfair_website/src/MainPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Row, Col} from 'react-materialize';
+import {Row, Col, Button} from 'react-materialize';
 
 class MainPage extends Component {
 
@@ -8,25 +8,40 @@ class MainPage extends Component {
         return (
             <div className="Container">
                 <Row className="App-header">
-                    <Col xl={12} l={12} m={12} className="App-Title">
+                    <Col xl={4} l={4} m={4} s={4} className="App-Title">
                         <div>
                             <h1>Meghan Fair</h1>
                         </div>
                     
                     </Col>
-                    <div className="Main-Page-Icons">
-                        <div>
-                            <i class="fas fa-home"></i>
-                        </div>
-                        <div>
-                            <i class="fab fa-github"></i>
-                        </div>
-                        <i class="fas fa-file-pdf"></i>
-                        <i class="fab fa-linkedin-in"></i>
-                    </div>
-                </Row>
-
-
+                    <Col className="Main-Page-Icons Header-Icons" xl={6} l={6} m={6} s={6}>
+                        <Col className="Main-Page-Icons " xl={2} l={2} m={2} s={2}>
+                            <Button class="Icon-Btn" flat='true' href="./MainPage.js">
+                                <i class="fas fa-home"></i>
+                            </Button>
+                        </Col>
+                        <Col className="Main-Page-Icons" xl={2} l={2} m={2} s={2}>
+                            <Button class="Icon-Btn" flat='true' href="../../about_me.html">
+                                <i class="fas fa-user"></i>
+                            </Button>
+                        </Col>
+                        <Col className="Main-Page-Icons" xl={2} l={2} m={2} s={2}>
+                            <Button class="Icon-Btn" flat='true'>
+                                <i class="fas fa-file-pdf"></i>
+                            </Button>
+                        </Col>
+                        <Col className="Main-Page-Icons" xl={2} l={2} m={2} s={2}>
+                            <Button class="Icon-Btn" flat='true'>
+                                <i class="fab fa-github"></i>
+                            </Button>
+                        </Col>
+                        <Col className="Main-Page-Icons" xl={2} l={2} m={2} s={2}>
+                            <Button class="Icon-Btn" flat='true'>
+                                <i class="fab fa-linkedin-in"></i>
+                            </Button>
+                        </Col>
+                    </Col>
+                </Row>                    
             </div>
         );
     }
