623ca4199a9741cace1ac67e6aa06144aff817dd
Meghan <mkfair05@gmail.com>
2020-03-27 15:51:51 -0700

    Finished adding sticky footer, choosing colours

--- react_website/src/StickyFooter.js
+++ react_website/src/StickyFooter.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+// import { createMuiTheme } from '@material-ui/core/styles';
 
 function Copyright() {
   return (
@@ -22,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '100vh',
+    minHeight: '10vh',
   },
   main: {
     marginTop: theme.spacing(8),
@@ -31,10 +32,26 @@ const useStyles = makeStyles((theme) => ({
   footer: {
     padding: theme.spacing(3, 2),
     marginTop: 'auto',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+    backgroundColor: '#dedede'
   },
 }));
+
+// const theme = createMuiTheme({
+//     palette: {
+//       primary: {
+//         light: '#757ce8',
+//         main: '#3f50b5',
+//         dark: '#002884',
+//         contrastText: '#fff',
+//       },
+//       secondary: {
+//         light: '#ff7961',
+//         main: '#f44336',
+//         dark: '#ba000d',
+//         contrastText: '#000',
+//       },
+//     },
+//   });
 
 export default function StickyFooter() {
   const classes = useStyles();
@@ -42,16 +59,6 @@ export default function StickyFooter() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <Container component="main" className={classes.main} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
-          Sticky footer
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          {'Pin a footer to the bottom of the viewport.'}
-          {'The footer will move as the main element of the page grows.'}
-        </Typography>
-        <Typography variant="body1">Sticky footer placeholder.</Typography>
-      </Container>
       <footer className={classes.footer}>
         <Container maxWidth="sm">
           <Typography variant="body1">Linked In  Github  Resume</Typography>
