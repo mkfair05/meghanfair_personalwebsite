0fad15ca64d83c1b3ed58c8cf5f427d8540776cd
Meghan <mkfair05@gmail.com>
2020-03-27 16:55:35 -0700

    Added header

--- react_website/src/StickyFooter.js
+++ react_website/src/StickyFooter.js
@@ -4,7 +4,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
-// import { createMuiTheme } from '@material-ui/core/styles';
 
 function Copyright() {
   return (
@@ -35,23 +34,6 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#dedede'
   },
 }));
-
-// const theme = createMuiTheme({
-//     palette: {
-//       primary: {
-//         light: '#757ce8',
-//         main: '#3f50b5',
-//         dark: '#002884',
-//         contrastText: '#fff',
-//       },
-//       secondary: {
-//         light: '#ff7961',
-//         main: '#f44336',
-//         dark: '#ba000d',
-//         contrastText: '#000',
-//       },
-//     },
-//   });
 
 export default function StickyFooter() {
   const classes = useStyles();
