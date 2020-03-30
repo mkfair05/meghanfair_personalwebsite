d7c359ef7c0b8f5f85fcf07f223bcadc05b01f40
Meghan <mkfair05@gmail.com>
2020-03-30 16:18:19 -0700

    Looking into adding photos. A bit of refactor.

--- react_website/src/App.js
+++ react_website/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StickyFooter from './StickyFooter';
-import Header from './Header'
+import Header from './Header';
+// import {Skeleton} from '@material-ui/lab';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      {/* <Skeleton variant="rect" width={500} height={500} animation="wave"/> */}
       <StickyFooter />
     </div>
   );
--- react_website/src/Header.js
+++ react_website/src/Header.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Fab from '@material-ui/core/Fab';
+import {
+  AppBar, Toolbar, Typography, makeStyles, CssBaseline, useScrollTrigger, Fab, Zoom
+} from '@material-ui/core';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import Zoom from '@material-ui/core/Zoom';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
